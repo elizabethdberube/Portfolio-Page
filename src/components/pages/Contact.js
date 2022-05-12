30d4a3bb59bff0db885b6b72abf6d3a57be1dbd8
elizabeth berube <elizabethdberube@gmail.com>
2022-05-12 14:40:18 -0400

    small chamge

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -21,9 +21,10 @@ function Contact() {
         } else {
             setNote(inputValue);
         }
+        handleValidation(event)
     };
 
-    const handleSubmit = (event) => {
+    const handleValidation = (event) => {
         event.preventDefault();
         if (!validateEmail(email)) {
             setErrorMessage('Email is invalid');
@@ -32,9 +33,7 @@ function Contact() {
         if (!name) {
             setErrorMessage('NAme is empty')
         }
-        setEmail('');
-        setName('');
-        setNote('');
+
     }
 
     return (
@@ -44,19 +43,19 @@ function Contact() {
                 <div className="form-group" >
                     <div className="form-group">
                         <label >Name</label>
-                        <input className="form-control" id="exampleInputPassword1" placeholder="Name" value={name} name="name" onChange={handleInput} type="name"></input>
+                        <input className="form-control" id="exampleInputPassword1" onMouseUp={handleInput} placeholder="Name" value={name} name="name" type="name"></input>
                     </div>
                     <label htmlFor="exampleInputEmail1">Email address</label>
-                    <input className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" type='email' name='email' onChange={handleInput} value={email}></input>
+                    <input className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" onMouseUp={handleInput} placeholder="Enter email" type='email' name='email' value={email}></input>
                     <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
                 </div>
                 <div className="form-group">
                     <label htmlFor="exampleFormControlTextarea1">Note</label>
-                    <textarea className="form-control" id="exampleFormControlTextarea1" rows="3" value={note} name="note" onChange={handleInput} type="note" placeholder="Note"></textarea>
+                    <textarea className="form-control" id="exampleFormControlTextarea1" rows="3" onMouseUp={handleInput} value={note} name="note" type="note" placeholder="Note"></textarea>
                 </div>
 
 
-                <button type="submit" className="btn btn-primary" onClick={handleSubmit}>Submit</button>
+                <button type="submit" className="btn btn-primary" >Submit</button>
 
             </form>
             <div>
